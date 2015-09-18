b31afb49de7655f4fb9f5184405738d724c98a65
coolsky0725 <coolsky0725@gmail.com>
2015-09-18 23:19:31 +0300

    InstaSong

---
+++ TermsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TermsViewController.h
+//  InstaSong
+//
+//  Created by zongda liang on 2/24/15.
+//  Copyright (c) 2015 com.liang.instasong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TermsViewController : UIViewController
+
+@end
